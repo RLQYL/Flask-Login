e58ac4fc6100112164f6d427c0462a948404f24d
Rick Lan <lanrick109@gmail.com>
2020-12-10 08:04:35 -0500

    Finished inserting the new account into the db

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///schedule.db"
 db = SQLAlchemy(app)
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     firstname = db.Column(db.String(10))
     lastname = db.Column(db.String(10))
-    username = db.Column(db.String(10), primary_key = True)
-    password = db.Column(db.String(10), primary_key = True)
+    username = db.Column(db.String(10))
+    password = db.Column(db.String(10))
 
 # class Priority(db.Model):
 #     id = db.Column(db.Integer, primary_key = True)
@@ -29,13 +29,26 @@ def index():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == "POST":
-        print(request.form)
+        username = request.form["username"]
+        password = request.form["password"]
+        user = User.query.filter_by(username=username, password=password).first()
+        print(user.id)
     return render_template('index.html')
 
 @app.route('/create_user', methods=['GET', 'POST'])
 def create_user():
     if request.method == "POST":
-        print(request.form)
+        username = request.form["username"]
+        password = request.form["password"]
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        if (User.query.filter_by(password=password).first() == None):
+            user = User(username=username,
+                        password=password,
+                        firstname=first_name,
+                        lastname=last_name)
+            db.session.add(user)
+            db.session.commit()
     return render_template('CreateAccount.html')   
 
 
